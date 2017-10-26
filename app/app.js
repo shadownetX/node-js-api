9fdd54d037dbf4d3b42191046f17ea32f0a6731a
vonP <thomas@thiebaudet.fr>
2017-10-26 05:53:10 +0200

    feat(init-api) add morgan logger

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 var express = require('express'),
     app = express(),
+    morgan = require('morgan'),
     port = process.env.PORT || 3000;
 
 console.log('App server started on: ' + port);
 
 app.listen(3000);
+
+app.use(morgan('dev'));
